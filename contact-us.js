2378e9e6a3024319f800f88fa0ee80fb13a58d8d
ginohmk <michotall95@gmail.com>
2024-03-11 14:32:46 +0100

    feat: add contact form validation and collection

---
+++ contact-us.js
@@ -0,0 +1,34 @@
+const email = document.querySelector('#email');
+const name = document.querySelector('#name');
+const message = document.querySelector('#message');
+const formSubmit = document.querySelector('#formSubmit');
+
+let userName = '';
+let userMessage = '';
+let userEmail = '';
+
+name.addEventListener('change', (event) => {
+  userName = event.target.value;
+});
+
+message.addEventListener('change', (event) => {
+  userMessage = event.target.value;
+});
+
+email.addEventListener('change', (event) => {
+  userEmail = event.target.value;
+});
+
+formSubmit.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  if (
+    userMessage.trim() === '' ||
+    userName.trim() === '' ||
+    userEmail.trim() === ''
+  ) {
+    alert('Please enter Values for fields ');
+  } else {
+    alert(`We have received your messsage ${userName}`);
+  }
+});
