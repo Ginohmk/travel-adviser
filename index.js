35d1ae9729cf41ce4202392d5b486010672e12ed
ginohmk <michotall95@gmail.com>
2024-03-14 12:36:18 +0100

    Feat: add search functionalities

--- index.js
+++ index.js
@@ -1,24 +1,78 @@
-import travelJson from './travel_recommendation_api.json';
+import { travelJson } from './travel_recommendation.js';
 
 const searcBtn = document.querySelector('#search-btn');
 const clearBtn = document.querySelector('#clear-btn');
 const searchInput = document.querySelector('#search-input');
+const ParentSearchValue = document.querySelector('#search-value-con');
 
-let searchInputHolder = '';
+let searchInputHolder;
 let searchResult = [];
 
+// Dynamic render Search val
+const renderSearchResult = (data, userInput) => {
+  let innerSearchValue;
+
+  if (data.length === 0) {
+    alert(
+      'Result not found, you can search for beaches, countries and temples'
+    );
+    innerSearchValue = ``;
+  } else {
+    let displayValue = [...data];
+
+    if (userInput === 'countries') {
+      displayValue = [];
+      let val = data.map(({ cities }) => cities);
+      val.forEach((el) => el.forEach((elInner) => displayValue.push(elInner)));
+    }
+
+    innerSearchValue = displayValue.map(
+      ({ name, id, imageUrl, description }) => `
+     <div class="search-value__item">
+     <div class="search-value__item-img-con">
+       <img src=${imageUrl} alt=${name} />
+     </div>
+
+     <div class="search-value__item-content">
+       <h3>${name}</h3>
+       <p>
+         ${description}
+       </p>
+       <a href="#" class="search-value__item-btn">view</a>
+     </div>
+   </div>
+`
+    );
+  }
+  ParentSearchValue.innerHTML = innerSearchValue;
+};
+
 searchInput.addEventListener('input', (event) => {
-  searchInputHolder = event.target.value;
+  searchInputHolder = event.target;
 });
 
 searcBtn.addEventListener('click', () => {
   // Search for match in travelJson
+  searchResult = [];
+  let userInput = searchInputHolder.value.trim().toLowerCase();
 
-  console.log(' Hello ');
+  if (userInput !== '') {
+    Object.entries(travelJson).forEach(([key, value]) => {
+      if (key === userInput) {
+        searchResult.push({ [key]: value });
+      }
+    });
 
-  searchResult = Object.entries(travelJson).forEach((key) => {
-    if (key === searchInputHolder.toLowerCase()) return { [key]: value };
-  });
+    let data = searchResult.length > 0 ? searchResult[0][`${userInput}`] : [];
 
-  console.log(searchResult, ' Search result ');
+    renderSearchResult(data, userInput);
+  } else {
+    alert('Search input is empty!!');
+  }
+});
+
+clearBtn.addEventListener('click', () => {
+  searchInputHolder.value = '';
+  searchResult = [];
+  renderSearchResult([]);
 });
